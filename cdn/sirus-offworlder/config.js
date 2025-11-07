39778768b0feecaba538d16609d1d150b62d5044
Oakshift Software <oakshiftsoftware@gmail.com>
2025-11-07 00:08:36 +0000

    Update config.js

--- cdn/sirus-offworlder/config.js
+++ cdn/sirus-offworlder/config.js
@@ -1,9 +1,5 @@
 // Sirus: Offworlder - JS Configuration File
 let statBlock = {
-  intelligence: 0,
-  charisma: 0,
-  strength: 0,
-  survival: 0,
-  health: 0,
-  speed: 0
+  intelligence: 0, charisma: 0, strength: 0,
+  survival: 0, health: 0, speed: 0
 };
