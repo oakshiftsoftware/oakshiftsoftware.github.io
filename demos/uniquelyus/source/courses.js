d3317970372d1694a171ad763889d4dc0240c837
Oakshift Software <oakshiftsoftware@gmail.com>
2026-06-06 22:53:51 +0100

    Add courses data for multiple courses
    Added courses data for various courses including titles, durations, spaces, prices, and URLs.

---
+++ demos/uniquelyus/source/courses.js
@@ -0,0 +1,17 @@
+const coursesData = [
+    {
+        id: "015", title: "Evening Crochet Course", 
+        weeks: 6, spaces: 0, price: 25.00, 
+        url: "https://bookwhen.com/uniquelyuswalkden/e/ev-se90m-20260608183000"
+    },
+    {
+        id: "016", title: "Painting Course", 
+        weeks: 6, spaces: 0, price: 32.50, 
+        url: "https://bookwhen.com/uniquelyuswalkden/e/ev-sx4sd-20260609183000"
+    },
+    {
+        id: "018", title: "Craft Course", 
+        weeks: 6, spaces: 8, price: 32.50, 
+        url: "https://bookwhen.com/uniquelyuswalkden/e/ev-s8ihp-20260617183000"
+    }
+];
